a240d293b1f48d7f70a8110100c383f15b13dcc8
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-12 19:46:34 +0200

    Add home page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Dragonet | Home</title>
+        <link rel="stylesheet" href="src/assets/styling/home.css">
+    </head>
+
+    <body>
+        <div class="flex-container">
+            <div class="">
+                <h1>Dragonet is a <span class="text-highlight">FOSS Support center</span> for companies alike. </h1>
+                <h2 class="subtitle">Support tickets? Articles? Integrations? Audit logs? <u>We have it all.</u></h2>
+                <a href="service.php"><button class="btn btn-primary">Interested?</button></a>
+                <a href="#"><button class="btn btn-primary">Get support</button></a>
+
+                <h3 style="display: inline-block" class="notice">© 2023 - <?php echo date("Y"); ?> by Blue Atomic</h3>
+            </div>
+            <img src="src/assets/images/BlueLogoJoshuaTransparent.png" alt="yay">
+        </div>
+    </body>
+</html>
